ccc4f612d7d0ef2152b094440c9cbba95707f543
Marco Terrinoni <marco.terrinoni@consoft.it>
2015-11-24 17:58:58 +0100

    Minor fix in hw6-3 walkthrough.

--- week_6_scalability/homeworks/hw6-3/hw6-3.js
+++ week_6_scalability/homeworks/hw6-3/hw6-3.js
@@ -7,7 +7,14 @@ sh.addShard("buchs:27020")
 // confirmation
 homework.check1()
 
-//
+// checks
+use config
+db.chunks.find( { ns:"week6.trades" }, {min:1,max:1,shard:1,_id:0} ).sort({min:1})
+// or
+db.chunks.aggregate( [
+ { $match : { ns : "week6.trades" } } ,
+ { $group : { _id : "$shard", n : { $sum : 1 } } }
+] )
 
 // run homework
 homework.c()
